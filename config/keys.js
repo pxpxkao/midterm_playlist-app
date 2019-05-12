e158fec57be61d28819aee0d5f27e926bfa7f20f
Kao Pei-Wei <pxpxkao1418@Kaos-MacBook-Air-4.local>
2019-05-12 23:38:49 +0800

    Version 1.0.0

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Kao:Kao12345@cluster0-w4tak.mongodb.net/test?retryWrites=truemongodb+srv://Kao:<password>@cluster0-w4tak.mongodb.net/test?retryWrites=true'
+    mongoURI: 'mongodb+srv://Kao:Kao12345@cluster0-w4tak.mongodb.net/test?retryWrites=true'
 }
